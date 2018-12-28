409630019faac2137e679b55157e2526b6b3a9df
jacob975 <z123a123s123@gmail.com>
2018-12-28 10:33:24 +0800

    Minor fix

--- calculate_extinction_curve_2.py
+++ calculate_extinction_curve_2.py
@@ -26,15 +26,19 @@ import phys_const
 from uncertainties import ufloat, umath
 
 # Calculate the ratio of A_lambda and Av
-def extinction_ratio(u_source, fitting):
-    A_lambda = -2.5 * (umath.log(u_source, 10.0) - umath.log(fitting, 10.0))
+def extinction_ratio(u_source, u_fitting, u_Av):
+    A_lambda = -2.5 * (umath.log(u_source, 10.0) - umath.log(u_fitting, 10.0))
+    ratio = A_lambda/u_Av
     return A_lambda 
 
 class process_Av_ratio():
-    def __init__(self, index_table, spectral_table, ratio_table):
+    def __init__(self, index_table, spectral_table, ratio_table, Av_table, chi_table):
         self.index_table = index_table
         self.spectral_table = spectral_table
         self.ratio_table = ratio_table
+        self.Av_table = Av_table
+        self.chi_table = chi_table
+        self.sqrt_chi_table = np.sqrt(chi_table)
         return
     def do(self, source_array, Q_array, SED_template):
         # Wipe out all Upper limit
@@ -46,13 +50,16 @@ class process_Av_ratio():
         err_ratio = np.zeros(len(source_array))
         for i, source in enumerate(source_array):
             u_source = ufloat(source[0], source[1])
+            u_Av = ufloat(self.Av_table[i,0], self.Av_table[i,1])
             fitting = ratio_table[i] * SED_template[index_table[i]]
-            #print (u_source, fitting)
-            u_ratio = extinction_ratio(u_source, fitting)
+            err_fitting = fitting * self.sqrt_chi_table[i]
+            u_fitting = ufloat(fitting, err_fitting)
+            u_ratio = extinction_ratio(u_source, u_fitting, u_Av)
             ratio[i] = u_ratio.n
             err_ratio[i] = u_ratio.s
         Av_ratio = np.average(ratio, weights = 1/err_ratio)
-        err_Av_ratio = np.divide(np.sqrt(np.sum(np.power(err_ratio, 2))), len(source))
+        var_Av_ratio = np.sum(np.divide(np.power(ratio - Av_ratio, 2), err_ratio))/np.sum(1/err_ratio)
+        err_Av_ratio = np.sqrt(var_Av_ratio)
         return Av_ratio, err_Av_ratio
 
 #--------------------------------------------
@@ -63,9 +70,9 @@ if __name__ == "__main__":
     start_time = time.time()
     #-----------------------------------
     # Load arguments
-    if len(argv) != 7:
+    if len(argv) != 8:
         print("The number of arguments is wrong.")
-        print("Usage: calculation_extinction_curve.py   [ source table] [ spectral index table] [ spectral table] [ ratio table] [ Av table] [ Q table]") 
+        print("Usage: calculation_extinction_curve.py   [ source table] [ spectral index table] [ spectral table] [ ratio table] [ Av table] [ Q table] [chi table]") 
         exit(1)
     source_table_name = argv[1]
     index_table_name = argv[2]
@@ -73,6 +80,7 @@ if __name__ == "__main__":
     ratio_table_name = argv[4]
     Av_table_name = argv[5]
     Q_table_name = argv[6]
+    chi_table_name = argv[7]
     #-----------------------------------
     # Load data
     source_table = np.loadtxt(source_table_name)
@@ -81,6 +89,7 @@ if __name__ == "__main__":
     ratio_table = np.loadtxt(ratio_table_name)
     Av_table = np.loadtxt(Av_table_name)
     Q_table = np.loadtxt(Q_table_name, dtype = str)
+    chi_table = np.loadtxt(chi_table_name)
     # Load template
     star_template = np.loadtxt("/mazu/users/Jacob975/ISM/20181227final/ISM_final_program/star_koornneef.dat", dtype = str)
     stage = star_template[:,0]
@@ -90,15 +99,17 @@ if __name__ == "__main__":
     # Load theoretical extinction curve
     WD55A = np.loadtxt('/mazu/users/Jacob975/ISM/20181227final/ISM_final_program/Synthetic_Extinction_Curves/WD55A.txt')
     WD31A = np.loadtxt('/mazu/users/Jacob975/ISM/20181227final/ISM_final_program/Synthetic_Extinction_Curves/WD31A_test.txt')
+    WD55B = np.loadtxt('/mazu/users/Jacob975/ISM/20181227final/ISM_final_program/Synthetic_Extinction_Curves/WD55B.txt')
     WD31B = np.loadtxt('/mazu/users/Jacob975/ISM/20181227final/ISM_final_program/Synthetic_Extinction_Curves/WD31B.txt')
     # Only take the data with Av > 0
-    Av_larger_than_0 = (Av_table[:,0] > 8.0)
+    Av_larger_than_0 = (Av_table[:,0] > 12.0)
     source_table = source_table[Av_larger_than_0]
     index_table = index_table[Av_larger_than_0]
     spectral_table = spectral_table[Av_larger_than_0]
     ratio_table = ratio_table[Av_larger_than_0]
     Av_table = Av_table[Av_larger_than_0]
     Q_table = Q_table[Av_larger_than_0]
+    chi_table = chi_table[Av_larger_than_0]
     # Take only ratio > 0
     ratio_larger_than_0 = ratio_table > 0.0
     source_table = source_table[ratio_larger_than_0]
@@ -107,10 +118,11 @@ if __name__ == "__main__":
     ratio_table = ratio_table[ratio_larger_than_0]
     Av_table = Av_table[ratio_larger_than_0]
     Q_table = Q_table[ratio_larger_than_0]
+    chi_table = chi_table[ratio_larger_than_0]
     print ('The number of source: {0}'.format(len(source_table)))
     #-----------------------------------
     # Calculate the extinction curve
-    stu = process_Av_ratio(index_table, spectral_table, ratio_table)
+    stu = process_Av_ratio(index_table, spectral_table, ratio_table, Av_table, chi_table)
     Av_ratio = np.zeros(len(band_array))
     err_Av_ratio = np.zeros(len(band_array))
     for i, band in enumerate(band_array):
@@ -120,20 +132,24 @@ if __name__ == "__main__":
         Q_table_lite = Q_table[:,i]
         Av_ratio[i], err_Av_ratio[i] = stu.do(source_array, Q_table_lite, SED_template_lite)
     Av_ratio = Av_ratio/Av_ratio[2]
+    err_Av_ratio = err_Av_ratio/Av_ratio[2]
     print (Av_ratio)
     print (err_Av_ratio)
     fig, axs = plt.subplots(1, 1)
     axs.errorbar(band_array[:,1], \
                 Av_ratio, \
                 yerr = err_Av_ratio, \
-                fmt = 'ro')
+                fmt = 'ro',
+                markersize = 2)
     # Normalized by K band
     WD31A_curve = WD31A[:,3]/5.566E-23
     WD55A_curve = WD55A[:,3]/7.547E-23
     WD31B_curve = WD31B[:,3]/5.925E-23
-    axs.plot(WD55A[:,0], WD55A_curve, label = 'W&D, $R_{v}$=5.5 A')
-    axs.plot(WD31A[:,0], WD31A_curve, linestyle = '--', label = 'W&D, $R_{v}$=3.1A')
-    #axs.plot(WD31B[:,0], WD31B_curve, label = 'W&D, $R_{v}$=3.1 B')
+    WD55B_curve = WD55B[:,3]/5.331E-23 
+    #axs.plot(WD55A[:,0], WD55A_curve, label = 'W&D, $R_{v}$=5.5A')
+    #axs.plot(WD31A[:,0], WD31A_curve, linestyle = '--', label = 'W&D, $R_{v}$=3.1A')
+    axs.plot(WD55B[:,0], WD55B_curve, label = 'W&D(2001), $R_{v}$=5.5B')
+    axs.plot(WD31B[:,0], WD31B_curve, label = 'W&D(2001), $R_{v}$=3.1B')
     axs.set_ylabel('$A_{\lambda}$/$A_{K}$')
     plt.xscale('log')
     plt.yscale('log')
